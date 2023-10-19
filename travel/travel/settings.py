ec946ad5a6e4279d183654eb17b53afef6ba2c2d
MinJoongWon <wnddnjs4593@gmail.com>
2023-10-19 14:45:48 +0900

    [add] html and connect to main.html

--- travel/travel/settings.py
+++ travel/travel/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'travelplan',
+    'user_app',
+    'crawling_app',
 ]
 
 MIDDLEWARE = [
---
+++ travel/travelplan/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+app_name = "travel_app"
+urlpatterns = [
+    path("", views.main, name="main"),]
